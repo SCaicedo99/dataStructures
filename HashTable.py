3c666203667e90e220a0328ff3ab50d67b2798fa
Sebastian Caicedo <43104210+SCaicedo99@users.noreply.github.com>
2018-11-19 19:40:09 -0500

    Initial commit

---
+++ HashTable.py
@@ -0,0 +1,84 @@
+# Hash tables work as a  dictionary, with a key to get to the data stored in them, they usually use the % to get the remainder and
+# use it as the storing place, in the best case scenario its running time is  o(1), but sometimes we have collisions, where more than
+# 1 value has the same key and it ends up being stored somewhere else in the table.
+class HashTable:
+    def __init__(self):
+        self.size = 11
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+    def put(self,key,data):
+      hashvalue = self.hashfunction(key,len(self.slots))
+
+      if self.slots[hashvalue] == None:
+        self.slots[hashvalue] = key
+        self.data[hashvalue] = data
+      else:
+        if self.slots[hashvalue] == key:
+          self.data[hashvalue] = data  #replace
+        else:
+          nextslot = self.rehash(hashvalue,len(self.slots))
+          while self.slots[nextslot] != None and \
+                          self.slots[nextslot] != key:
+            nextslot = self.rehash(nextslot,len(self.slots))
+
+          if self.slots[nextslot] == None:
+            self.slots[nextslot]=key
+            self.data[nextslot]=data
+          else:
+            self.data[nextslot] = data #replace
+
+    def hashfunction(self,key,size):
+         return key % size
+
+    def rehash(self,oldhash,size):
+        return (oldhash+1) % size
+
+    def get(self,key):
+      startslot = self.hashfunction(key,len(self.slots))
+
+      data = None
+      stop = False
+      found = False
+      position = startslot
+      while self.slots[position] != None and  \
+                           not found and not stop:
+         if self.slots[position] == key:
+           found = True
+           data = self.data[position]
+         else:
+           position=self.rehash(position,len(self.slots))
+           if position == startslot:
+               stop = True
+      return data
+
+    def __getitem__(self,key):
+        return self.get(key)
+
+    def __setitem__(self,key,data):
+        self.put(key,data)
+
+H=HashTable()
+H[54]="cat"
+H[26]="dog"
+H[93]="lion"
+H[17]="tiger"
+H[77]="bird"
+H[31]="cow"
+H[44]="goat"
+H[55]="pig"
+print(H[55])
+H[55]="test" #TEst
+print(H[55])
+H[20]="chicken"
+print(H.slots)
+print(H.data)
+
+print(H[20])
+
+print(H[17])
+H[20]='duck'
+print(H[20])
+print(H[99])
+print(H.slots)
+print(H.data)
